3b4fa8317f8067b6cf9d7f079b95127dbe4b5cfb
Joaquin Vesco Aparicio <joaquinvesapa@gmail.com>
2022-12-30 13:08:57 -0300

    bg violet with gradient circles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'dk-violet': '#262175',
+				'pink': '#ED63F2',
+				'lt-green': '#6DD68B',
+				'dk-grey': '#212121',
+				'lt-white': '#E1E5CE'
+			  },
+		},
 	},
 	plugins: [],
 }
