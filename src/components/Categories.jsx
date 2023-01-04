b7dc83c5f82b5b5d26c81810ed110fd6176af9b6
Joaquin Vesco Aparicio <joaquinvesapa@gmail.com>
2023-01-04 16:29:23 -0300

    filtros responsive

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -5,8 +5,16 @@ const Categories = (props) => {
     const { categories, selectedCategory, onCategorySelect} = props;
     return (
       <div className='relative top-5'>
-      <RadioGroup className="flex top-5 flex-row justify-center" onChange={event => onCategorySelect(event)} value={selectedCategory}>
-        {categories.map(category => (
+      <RadioGroup className="flex mb-3 flex-row justify-center" onChange={event => onCategorySelect(event)} value={selectedCategory}>
+        {categories.slice(0,3).map(category => (
+          <RadioGroup.Option key={category} value={category} className={`drop-shadow-df text-white font-bold text-xl mx-3 rounded-lg border-lt-white px-4
+           ${category === selectedCategory ? 'bg-lt-green' :  'bg-dk-grey'}`}>{category}
+            
+          </RadioGroup.Option>
+        ))}
+      </RadioGroup>
+      <RadioGroup className="flex top-10 flex-row justify-center" onChange={event => onCategorySelect(event)} value={selectedCategory}>
+        {categories.slice(3,6).map(category => (
           <RadioGroup.Option key={category} value={category} className={`drop-shadow-df text-white font-bold text-xl mx-3 rounded-lg border-lt-white px-4
            ${category === selectedCategory ? 'bg-lt-green' :  'bg-dk-grey'}`}>{category}
             
